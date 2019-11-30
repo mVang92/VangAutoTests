2710dc31be1c44358cdabf9140ee1ef4a64f3f89
mVang92 <meng.vang92@yahoo.com>
2019-11-30 12:02:37 -0600

    add test to verify bug fix for signUp and signIn

--- src/baseClass/BaseClass.java
+++ src/baseClass/BaseClass.java
@@ -3,11 +3,15 @@ package baseClass;
 public class BaseClass {
 	public String chromeDriverPath = "C:\\chromedriver\\chromedriver.exe";
 	
-	public String signIn = "//*[contains(text(),'Sign In')]";
-	public String emailTextInput = "//input[@type=\"text\"]";
-	public String passwordTextInput = "//input[@type=\"password\"]";
-	public String signInButton = "//button[@type=\"submit\"]";
-	public String signOutButton = "//div[contains(text(),'Sign Out')]";
-	public String confirmSignOut = "//button[contains(text(),\"Yes\")]";
+	public String signIn = "signInNavButton";
+	public String signUp = "signUpNavButton";
+	public String emailTextInput = "emailInput";
+	public String passwordTextInput = "passwordInput";
+	public String signInButton = "signInButton";
+	public String signOutButton = "signOutNavButton";
+	public String signUpButton = "signUpButton";
+	public String confirmSignOut = "doSignOutButton";
+	public String closeSignUpModal = "closeSignUpModal";
 	public String userEmail = "//span[@id=\"userEmail\"]/text()[1]";
+	public String toastNotificationError = "//*[@class='Toastify__toast Toastify__toast--error']";
 }
