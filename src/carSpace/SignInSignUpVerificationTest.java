273681576889aa64c8a3f48600b814d33cb6e5fe
Meng Vang <meng.vang92@yahoo.com>
2019-12-01 12:52:08 -0600

    indented code that for some reason, did not indent in the repo

--- src/carSpace/SignInSignUpVerificationTest.java
+++ src/carSpace/SignInSignUpVerificationTest.java
@@ -21,24 +21,24 @@ public class SignInSignUpVerificationTest extends BaseClass {
 		System.setProperty("webdriver.chrome.driver", chromeDriverPath);
 		WebDriver driver = new ChromeDriver();
 		maximizeWindow(driver);
-        driver.get(carSpaceUrl);
-        clickOnElementUsingId(driver, signUp);
-        fillInputFieldUsingId(driver, emailTextInput, userEmail);
-        clickOnElementUsingId(driver, signUpButton);
-        Thread.sleep(1000);
-        String expectedMessage = invalidPasswordErrorMessage;
+		driver.get(carSpaceUrl);
+		clickOnElementUsingId(driver, signUp);
+		fillInputFieldUsingId(driver, emailTextInput, userEmail);
+		clickOnElementUsingId(driver, signUpButton);
+		Thread.sleep(1000);
+		String expectedMessage = invalidPasswordErrorMessage;
 		String actualMessage = getTextUsingXpath(driver, toastNotificationBody);
 		assertTrue(actualMessage.contains(expectedMessage));
 		clickOnElementUsingXpath(driver, toastNotificationCloseButton);
-        driver.findElement(By.id(closeSignUpModal)).click();
-        driver.findElement(By.id(signIn)).click();
-        driver.findElement(By.id(passwordTextInput)).sendKeys(password);
-        driver.findElement(By.id(signInButton)).click();
-        Thread.sleep(1000);
-        expectedMessage = invalidEmailErrorMessage;
+		driver.findElement(By.id(closeSignUpModal)).click();
+		driver.findElement(By.id(signIn)).click();
+		driver.findElement(By.id(passwordTextInput)).sendKeys(password);
+		driver.findElement(By.id(signInButton)).click();
+		Thread.sleep(1000);
+		expectedMessage = invalidEmailErrorMessage;
 		actualMessage = getTextUsingXpath(driver, toastNotificationBody);
 		assertTrue(actualMessage.contains(expectedMessage));
 		clickOnElementUsingXpath(driver, toastNotificationCloseButton);
-        driver.close();
+		driver.close();
 	}
 }
