7578f202f4e3b76378522a139a315999a2287247
mVang92 <meng.vang92@yahoo.com>
2020-05-30 00:16:51 -0500

    add service log tests

--- src/carSpace/LogPage/AddServiceLogNegativeTest.java
+++ src/carSpace/LogPage/AddServiceLogNegativeTest.java
@@ -2,6 +2,7 @@ package carSpace.LogPage;
 
 import static org.testng.Assert.assertTrue;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import baseClass.BaseClass;
@@ -10,26 +11,60 @@ public class AddServiceLogNegativeTest extends BaseClass {
 	private int year = 2014;
 	private String make = "Honda";
 	private String model = "Accord";
+	private String date = "01012020";
+	private String service = "Transmission Fluid Change";
 	
 	@BeforeClass
 	private void setup() {
 		doSignIn();
 		addVehicle(year, make, model);
+		selectVehicle(year, make, model);
 	}
 	
 	/**
-	 * Verify the functionality of the error message while trying to submit empty
-	 * input fields when adding a service log
+	 * Verify the error message when trying to submit an empty input field
 	 */
-	@Test
-	private void addServiceLogNegativeTest() {
-		selectVehicle(year, make, model);
-		fillInputField(serviceLogDateInput, "01012020", id);
-		fillInputField(serviceLogServiceInput, "Transmission Fluid Change", id);
+	@Test(priority = 0)
+	private void missingInputFieldsTest() {
+		fillInputField(serviceLogDateInput, date, id);
+		fillInputField(serviceLogServiceInput, service, id);
 		clickOnElement(addServiceLogButton, id);
 		String actualMessage = getText(addLogErrorModal, xpath);
 		assertTrue(actualMessage.contains(addLogsMissingFieldsErrorMessage));
+	}
+	
+	/**
+	 * Verify the user cannot add a service log if the mileage and service inputs contain only spaces
+	 */
+	@Test(priority = 1)
+	private void emptyInputFieldsTest() {
+		String inputWithSpaces = "     ";
+		fillInputField(serviceLogDateInput, date, id);
+		fillInputField(serviceLogMileageInput, inputWithSpaces, id);
+		fillInputField(serviceLogServiceInput, inputWithSpaces, id);
+		clickOnElement(addServiceLogButton, id);
+		String actualMessage = getText(addLogErrorModal, xpath);
+		assertTrue(actualMessage.contains(addLogsMissingFieldsErrorMessage));
+	}
+	
+	/**
+	 * Verify the error message when adding an invalid mileage input
+	 */
+	@Test(priority = 2)
+	private void invalidMileageInputTest() {
+		String invalidMileage = "nan";
+		fillInputField(serviceLogDateInput, date, id);
+		fillInputField(serviceLogMileageInput, invalidMileage, id);
+		fillInputField(serviceLogServiceInput, service, id);
+		clickOnElement(addServiceLogButton, id);
+		String actualMessage = getText(addLogErrorModal, xpath);
+		assertTrue(actualMessage.contains(invalidMileage + " is not a valid input for Mileage."));
+	}
+	
+	@AfterMethod
+	private void resetVehicleInputFields() {
 		clickOnElement(addLogErrorModalOkayButton, xpath);
+		clickOnElement(addLogResetInputFieldsButton, id);
 	}
 	
 	@AfterClass
