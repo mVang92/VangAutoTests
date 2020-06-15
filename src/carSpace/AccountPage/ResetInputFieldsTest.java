3aa746e0e038ceff8f09893e7077ebdb42af8736
mVang92 <meng.vang92@yahoo.com>
2020-06-15 01:01:51 -0500

    fix indentation

--- src/carSpace/AccountPage/ResetInputFieldsTest.java
+++ src/carSpace/AccountPage/ResetInputFieldsTest.java
@@ -10,61 +10,61 @@ import baseClass.BaseClass;
 public class ResetInputFieldsTest extends BaseClass {
 	private String picture = "https://oswallpapers.com/wp-content/uploads/2016/06/img19.jpg";
 	private String displayName = "Sally Thing";
-	
+
 	@DataProvider(name = "testData")
-    public Object[][] dataProviderMethod() {
-        return new Object[][]
-        {
-        	{ 
-        		newBackgroundPictureInput,
-        		submitNewBackgroundPictureButton,
-        		expectedUpdateBackgroundPictureModalTitle,
-        		resetNewBackgroundPictureButton,
-        		expectedDefaultBackgroundPictureModalTitle,
-        		picture,
-        		closeUpdatePictureModalButton
-        	},
-        	{ 
-        		newProfilePictureInput,
-        		submitNewProfilePictureButton,
-        		expectedUpdateProfilePictureModalTitle,
-        		resetNewProfilePictureButton,
-        		expectedDefaultPictureModalTitle,
-        		picture,
-        		closeUpdatePictureModalButton
-        	},
-        	{ 
-        		newDisplayNameInput,
-        		submitNewDisplayNameButton,
-        		"Use \"" + displayName + "\" as your name?",
-        		resetNewDisplayNameButton,
-        		expectedDefaultNameModalTitle,
-        		displayName,
-        		closeUpdateDisplayNameModalButton
-        	}
-        };
-    }
-	
+	public Object[][] dataProviderMethod() {
+		return new Object[][] {
+			{
+				newBackgroundPictureInput,
+				submitNewBackgroundPictureButton,
+				expectedUpdateBackgroundPictureModalTitle,
+				resetNewBackgroundPictureButton,
+				expectedDefaultBackgroundPictureModalTitle,
+				picture,
+				closeUpdatePictureModalButton
+			},
+			{
+				newProfilePictureInput,
+				submitNewProfilePictureButton,
+				expectedUpdateProfilePictureModalTitle,
+				resetNewProfilePictureButton,
+				expectedDefaultPictureModalTitle,
+				picture,
+				closeUpdatePictureModalButton
+			},
+			{
+				newDisplayNameInput,
+				submitNewDisplayNameButton,
+				"Use \"" + displayName + "\" as your name?",
+				resetNewDisplayNameButton,
+				expectedDefaultNameModalTitle,
+				displayName,
+				closeUpdateDisplayNameModalButton
+			}
+		};
+	}
+
 	@BeforeClass
 	private void setup() {
 		doSignIn();
 		clickOnElement(menuDropdownButton, id);
 		clickOnElement(accountNavButton, id);
 	}
-	
+
 	/**
-	 * Verify the Reset function works for the update input fields and the react states reset each field
+	 * Verify the Reset function works for the update input fields and the react
+	 * states reset each field
 	 */
 	@Test(dataProvider = "testData")
 	public void updateBackgroundPictureResetInputFieldTest(
-			String inputField,
-			String submitButton,
-			String expectedModalTitle,
-			String resetButton,
-			String expectedDefaultModalTitle,
-			String inputValue,
-			String closeModalButton
-		) {
+		String inputField,
+		String submitButton,
+		String expectedModalTitle,
+		String resetButton,
+		String expectedDefaultModalTitle,
+		String inputValue,
+		String closeModalButton
+	) {
 		assertEquals(getValue(inputField, id), "");
 		fillInputField(inputField, inputValue, id);
 		clickOnElement(submitButton, id);
@@ -75,7 +75,7 @@ public class ResetInputFieldsTest extends BaseClass {
 		assertEquals(getText(modalTitle, xpath), expectedDefaultModalTitle);
 		clickOnElement(closeModalButton, id);
 	}
-	
+
 	@AfterClass(alwaysRun = true)
 	private void teardown() {
 		close();
