f627ad170c42c29a37171ad07cd49a75e13e91ee
mVang92 <meng.vang92@yahoo.com>
2020-08-04 22:42:43 -0500

    changes to portfolio tests

--- src/mVangPortfolio/ResetCommentBoxTest.java
+++ src/mVangPortfolio/ResetCommentBoxTest.java
@@ -2,20 +2,23 @@ package mVangPortfolio;
 
 import static org.testng.Assert.assertEquals;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import basePage.BasePage;
 
 public class ResetCommentBoxTest extends BasePage {
+	
+	@BeforeClass
+	private void setup() {
+		doPortfolioTest();
+	}
 
 	/**
 	 * Verify the functionality of the reset button in the comment box
 	 */
 	@Test
 	private void resetCommentBoxTest() {
-		setProperty();
-		maximizeWindow();
-		usePortfolioUrl();
 		clickOnElement(contactNavButton, xpath);
 		clickOnElement(portfolioResetButton, id);
 		fillInputField(portfolioCommentInput, "This is a test comment from VangAutoTests.", id);
