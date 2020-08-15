4af31421c206bd31731f0757dc630ce3d4d54942
mVang92 <meng.vang92@yahoo.com>
2020-08-14 23:24:15 -0500

    add test for editing vehicle info

--- src/carSpace/LogPage/EditVehicleInfoPositiveTest.java
+++ src/carSpace/LogPage/EditVehicleInfoPositiveTest.java
@@ -3,37 +3,82 @@ package carSpace.LogPage;
 import static org.testng.Assert.assertEquals;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import basePage.BasePage;
 
 public class EditVehicleInfoPositiveTest extends BasePage {
-	private int year = 2014;
+	private String year = "2014";
 	private String make = "Honda";
 	private String model = "Pilot";
-	private int newYear = 1999;
+	private String newYear = "1999";
 	private String newMake = "Lexus";
 	private String newModel = "ES 300";
 	
-	@BeforeClass
-	private void setup() {
-		doSignIn();
-		addVehicle(year, make, model);
-		selectVehicle(year, make, model);
+	@DataProvider(name = "data")
+	public Object[][] dataProviderMethod() {
+		return new Object[][] {
+			{
+				year,
+				make,
+				model,
+				newYear,
+				vehicleYearInput				
+			},
+			{
+				newYear,
+				make,
+				model,
+				newMake,
+				vehicleMakeInput
+			},
+			{
+				newYear,
+				newMake,
+				model,
+				newModel,
+				vehicleModelInput				
+			},
+			{
+				newYear,
+				newMake,
+				newModel,
+				null,
+				null				
+			}
+		};
 	}
 	
-	@Test
-	private void test() {
+	@BeforeClass
+	public void setup() {
+		doSignIn();
+		addVehicle(Integer.parseInt(year), make, model);
+	}
+	
+	/**
+	 * Verify the vehicle name, make, and model are editable and the user is able to save them
+	 */
+	@Test(dataProvider = "data")
+	public void changeVehicleNameTest(
+		String year,
+		String make,
+		String model,
+		String input,
+		String inputField		
+	){
+		selectVehicle(Integer.parseInt(year), make, model);
 		assertEquals(getText(vehicleNameHeader, id), year + " " + make + " " + model);
-		clickOnElement(editVehicleNameButton, id);
-		fillInputField(vehicleYearInput, newYear, id);
-		clickOnElement(confirmSaveEditVehicleNameButton, id);
-		System.out.println(getText(vehicleNameHeader, id));
-		assertEquals(getText(vehicleNameHeader, id), newYear + " " + make + " " + model);
+		if (input != null) {
+			clickOnElement(editVehicleNameButton, id);
+			fillInputField(inputField, input, id);
+			clickOnElement(confirmSaveEditVehicleNameButton, id);
+			clickOnElement(applicationName, id);
+		}
 	}
 
 	@AfterClass
-	private void teardown() {
-		deleteVehicle(year, make, model);
+	public void teardown() {
+		deleteVehicle(Integer.parseInt(newYear), newMake, newModel);
 		close();
 	}
 
