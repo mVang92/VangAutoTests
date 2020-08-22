9ad0446401f6dfb7a97685a0f70ce36111fabeed
mVang92 <meng.vang92@yahoo.com>
2020-08-21 21:50:12 -0500

    removed unised import

--- src/carSpace/authentication/LoggedOutTest.java
+++ src/carSpace/authentication/LoggedOutTest.java
@@ -1,6 +1,5 @@
 package carSpace.authentication;
 
-import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 import org.testng.annotations.AfterClass;
