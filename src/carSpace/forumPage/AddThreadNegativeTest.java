a62e98fb2a6da9b3187565a8a13972bd83873426
mVang92 <meng.vang92@yahoo.com>
2020-09-05 23:05:39 -0500

    add negative test for adding thread

---
+++ src/carSpace/forumPage/AddThreadNegativeTest.java
@@ -0,0 +1,43 @@
+package carSpace.forumPage;
+
+import static org.testng.Assert.assertEquals;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import basePage.BasePage;
+
+public class AddThreadNegativeTest extends BasePage {
+	
+	@BeforeClass
+	private void setup() {
+		doSignIn();
+		clickOnElement(menuDropdownButton, id);
+		clickOnElement(forumNavButton, id);
+		clickOnElement(startNewThreadButton, id);
+	}
+	
+	/**
+	 * Verify the error message when trying to submit a thread with empty input fields
+	 */
+	@Test
+	private void missingInputFieldsTest() {
+		clickOnElement(submitNewThreadButton, id);
+		assertEquals(getText(toastNotificationBody, xpath), addThreadMissingFieldsErrorMessage);
+		clickOnElement(toastNotificationErrorCloseButton, xpath);
+		fillInputField(newThreadTitleInput, "Title", id);
+		clickOnElement(submitNewThreadButton, id);
+		assertEquals(getText(toastNotificationBody, xpath), addThreadMissingFieldsErrorMessage);
+		clickOnElement(toastNotificationErrorCloseButton, xpath);
+		fillInputField(newThreadTitleInput, " ", id);
+		fillInputField(newThreadDescriptionInput, "Description", id);
+		clickOnElement(submitNewThreadButton, id);
+		assertEquals(getText(toastNotificationBody, xpath), addThreadMissingFieldsErrorMessage);
+		clickOnElement(toastNotificationErrorCloseButton, xpath);
+	}
+	
+	@AfterClass
+	private void teardown() {
+		close();
+	}
+
+}
