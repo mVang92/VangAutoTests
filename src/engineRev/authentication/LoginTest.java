d1dd7fc7b3d112155904ca844556d3c4db14e740
mVang92 <meng.vang92@yahoo.com>
2020-12-12 22:54:54 -0600

    modify login test to check main page displayName

--- src/engineRev/authentication/LoginTest.java
+++ src/engineRev/authentication/LoginTest.java
@@ -2,18 +2,24 @@ package engineRev.authentication;
 
 import static org.testng.Assert.assertEquals;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 import basePage.BasePage;
 
 public class LoginTest extends BasePage {
+	
+	@BeforeClass
+	public void setup() {
+		doEngineRevTest();
+	}
 
 	/**
 	 * Verify the user can sign-in and sign-out, and the email displays correctly
 	 */
 	@Test
 	private void loginTest() {
-		doSignIn();
+		signIn(getProp("testUser"), getProp("testUserPassword"));
+		assertEquals(getText(displayName, id), getProp("testUserDisplayName"));
 		clickOnElement(menuDropdownButton, id);
 		clickOnElement(accountNavButton, id);
 		assertEquals(getText(accountPageUserEmail, id), getProp("testUser"));
